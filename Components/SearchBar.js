a1a91cc2285928f371c3f3db931950a4cb403b25
Shivam Sharma <emshivam01@gmail.com>
2023-03-30 21:33:53 +0530

    changes in file structure

---
+++ Components/SearchBar.js
@@ -0,0 +1,11 @@
+function SearchBar() {
+  return (
+    <input
+      className="SearchBar"
+      type="text"
+      placeholder="Search for restaurants, cuisines or a dish..."
+    />
+  );
+}
+
+export default SearchBar;
