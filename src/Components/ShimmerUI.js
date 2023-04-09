021337c21e2e31a7021e8662a7caaa7c9b17d5c5
Shivam Sharma <emshivam01@gmail.com>
2023-04-09 12:02:51 +0530

    added styling to shimmer ui component

--- src/Components/ShimmerUI.js
+++ src/Components/ShimmerUI.js
@@ -1,5 +1,13 @@
 function ShimmerUI() {
-  return <div className="shimmer-ui">UI Loading....</div>;
+  return (
+    <div className="Cards shimmer-ui">
+      {Array(10)
+        .fill(" ")
+        .map((i) => (
+          <div className="shimmer-Cards"></div>
+        ))}
+    </div>
+  );
 }
 
 export default ShimmerUI;
