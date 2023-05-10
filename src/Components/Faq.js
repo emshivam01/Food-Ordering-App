bde5d23ec3acadf96023070a3a2427db771a0805
Shivam Sharma <emshivam01@gmail.com>
2023-05-10 13:01:00 +0530

    Writing logic for our accordian

--- src/Components/Faq.js
+++ src/Components/Faq.js
@@ -1,28 +1,59 @@
-const para =
-  "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat";
+import { useState } from "react";
 
-const Section = ({ name, para }) => {
+const para =
+  "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.";
+
+const Section = ({ name, para, isVisible, setIsVisible }) => {
   return (
-    <div className="border-2 border-gray-400 shadow-lg my-10 mx-12 p-2">
+    <div className="bg-[#118091] border-2 border-gray-300 shadow-lg m-10 p-2 px-4 rounded-lg">
       <div className="flex justify-between">
-        <h1 className="text-2xl font-semibold underline underline-offset-4 mb-4">
-          {name}
-        </h1>
-        <button className="text-2xl font-semibold underline underline-offset-4 mb-4 cursor-pointer">
-        <span className="material-symbols-outlined text-2xl font-semibold mb-4 cursor-pointer">
-        arrow_drop_down
-        </span>
+        <h1 className="text-2xl font-semibold text-white">{name}</h1>
+        <button
+          onClick={() => {
+            isVisible ? setIsVisible(false) : setIsVisible(true);
+          }}
+          className="text-2xl font-semibold cursor-pointer"
+        >
+          <span className="material-symbols-rounded text-4xl font-semibold text-white cursor-pointer">
+            {isVisible ? "arrow_drop_up" : "arrow_drop_down"}
+          </span>
         </button>
       </div>
-      <p className="text-lg font-normal">{para}</p>
+      {isVisible && (
+        <p className="text-lg font-normal text-white mt-3">{para}</p>
+      )}
     </div>
   );
 };
 
 const Faq = () => {
+  const [visibleSec, setVisibleSec] = useState("second");
   return (
     <div>
-      <Section name={"Shivam Sharma"} para={para} />
+      <Section
+        name={"First FAQ "}
+        para={para}
+        isVisible={visibleSec === "first"}
+        setIsVisible={() => {
+          setVisibleSec("first");
+        }}
+      />
+      <Section
+        name={"Second FAQ "}
+        para={para}
+        isVisible={visibleSec === "second"}
+        setIsVisible={() => {
+          setVisibleSec("second");
+        }}
+      />
+      <Section
+        name={"Third FAQ "}
+        para={para}
+        isVisible={visibleSec === "third"}
+        setIsVisible={() => {
+          setVisibleSec("third");
+        }}
+      />
     </div>
   );
 };
